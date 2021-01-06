6a0e7303e1e82f0b6bf4b34afee2f79135340026
David Khvedelidze <45176754+khvedela@users.noreply.github.com>
2021-01-06 21:00:56 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,18 +2,14 @@ import discord
 from discord.ext import commands
 import os
 from discord.utils import get
-
+import random
 
 client = discord.Client()
 
-DISCORD_TOKEN = os.getenv("TOKEN")
+DISCORD_TOKEN = os.getenv("NzU5MDg0ODE1OTM2MTI3MDE2.X24W0w.EiuI4wVfn812nNSjooDblH26gw8")
 
 
 bot = commands.Bot(command_prefix="#") #PREFIX FOR BOT
-
-@client.event
-async def on_ready():
-    print('BOT ACTIVATED')
 
 #MAKE BOT JOIN VC
 @bot.command()
@@ -58,8 +54,37 @@ async def chnick(ctx, member: discord.Member, nick):
 
 
 @bot.command()
-async def sheminde(ctx):
-    await ctx.send("iphone 12 pro max miyide da shedebuligaq")
+async def penis(ctx):
+    count = random.randint(1, 15)
+    count_king = random.randint(15, 20)
+    count_sandro = random.randint(1, 4)
+    size = '=' * count
+    size_king = '=' * count_king
+    size_sandro = '=' * count_sandro
+    if ctx.author.id == 271350836179763201:
+        await ctx.send(f'hi king! this is your dick -> 8{size_king}D')
+    elif ctx.author.id == 517551037977067520:
+        await ctx.send(f'hi sandro! this is your dick -> 8{size_sandro}D')
+    elif ctx.author.id == 790881474298642482:
+        await ctx.send('girl error')
+    elif ctx.author.id == 370255524966563853:
+        await ctx.send('girl error')
+    elif ctx.author.id == 760938532927176706:
+        await ctx.send('girl error')
+    else:
+        await ctx.send(f'hi! this is your dick -> 8{size}D')
+    
 
 
-bot.run("TOKEN")
+@bot.command()
+async def howgay(ctx):
+    count = random.randint(0, 100)
+    count_sandro = random.randint(120, 150)
+    count_king = random.randint(0, 9)
+    if ctx.author.id == 517551037977067520:
+        await ctx.send(f'sandro you are {count_sandro}% gay')
+    elif ctx.author.id == 271350836179763201:
+        await ctx.send(f'hi king you are {count_king}% gay')
+    else:
+        await ctx.send(f'hi king you are {count}% gay')
+bot.run("NzU5MDg0ODE1OTM2MTI3MDE2.X24W0w.EiuI4wVfn812nNSjooDblH26gw8")
